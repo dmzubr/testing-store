1b04f1d27aa82d5be477f33f2fe88d91d4d9466c
AndreyRomanenko <simulator00@gmail.com>
2016-09-21 16:05:34 +0300

    release 1.0.0.7 1.0.0.8 1.0.0.9
    Добавил angular-toastr в проект, подцепил Font Awesome, добавил скрипты
    в контроллер scenario для добавления сущности в таблицу scenario,
    добавил во вью список с кнопками Edit/Delete с помощью Font Awesome,
    добавил кнопку Add для добавления сущности в таблицу. Кнопка Edit во вью
    пока не работает.

--- app/scripts/controllers/ctrlscenario.js
+++ app/scripts/controllers/ctrlscenario.js
@@ -8,8 +8,10 @@
  * Controller of the testerApp
  */
 angular.module('testerApp')
-  .controller('CtrlScenario', [ '$scope', 'scenarioFactory', function ($scope,scenarioFactory) {
+  .controller('CtrlScenario', [ '$scope', '$http','scenarioFactory', function ($scope,$http,scenarioFactory) {
     
+  	var _urlLink = 'http://localhost:8085/scenario'
+
     function getScenarioList(){
       scenarioFactory.GetScenarioList()
       	.then(function(res){
@@ -21,7 +23,50 @@ angular.module('testerApp')
       	})	
     };
 
+    
+    function postDataToTable(scenarioData){
+       if (!scenarioData.ScenarioId){
+       	$http.post(_urlLink, scenarioData)
+       	 .then(function(res){
+       	 	getScenarioList();
+       	 	toastr.success('Ok', 'good');
+       	 }, function(error){
+       	 	toastr.error('Плохой код', 'Поправь его');
+
+       	 })
+
+       };
+     	
+
+    };
+
+    function _getUrlScenario(getData){
+       return _urlLink + '(' + getData.ScenarioId + ')'
+       console.log(_urlLink);
+    };
+
+    function deleteScenario(deletedScenario) { 
+        //debugger; 
+        var targetUrl = _getUrlScenario(deletedScenario); 
+        //console.log('targetUrl is ' + targetUrl); 
+
+        $http.delete(targetUrl) 
+	        .then(function(res) { 
+	        	angular.forEach($scope.scenarioList, function(element, index) { 
+	        		if (element == deletedScenario) 
+	        		$scope.scenarioList.splice(index, 1); 
+					debugger; 		
+	        	}); 
+        		toastr.success('Ok', 'good');
+        		}, function(error) { 
+        			//bugger; 
+       				 toastr.error('Плохой код', 'Поправь его');
+        		   }); 
+    } 
+
     getScenarioList();
+    $scope.postDataToTable = postDataToTable;
+    $scope.deleteScenario = deleteScenario;
 
 
   }]);
